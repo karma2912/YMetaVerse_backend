1628a5caa07f1871b8ac8dddce5de1f7fc91f68e
karma2912 <rajakyash23@gmail.com>
2024-12-18 02:21:09 +0530

    commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const {Server, Socket} = require("socket.io")
+const connectToMongo = require("./db.js")
+const app = express()
+const cors = require("cors")
+connectToMongo()
+
+
+// here are the api's which were made using express.js
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/auth", require("./routes/auth"))
+
+app.get("/",(req,res)=>{
+    res.send("Hello Karma I am from Backend")
+})
+
+const server = app.listen(4000,()=>{
+    console.log("App is listening on port 4000")
+})
+
+const io = new Server(server,{
+    cors:true
+})
+
+// here are the connection made by socket.io
+
+const nameTosocketMapping = new Map()
+
+const socketTonameMapping = new Map()
+
+const nameToPositionMapping = new Map()
+
+let players = {}
+ 
+io.on("connection",(socket)=>{
+    socket.on("new-player",(data)=>{
+        const { name,x,y } = data
+        let playerPositon = {x,y}
+        players[name] = playerPositon
+        console.log(players)
+        nameToPositionMapping.set(name,playerPositon)
+        nameTosocketMapping.set(name,socket.id)
+        socketTonameMapping.set(socket.id,name)
+        socket.broadcast.emit("User-joined",name)
+    })
+    socket.on("Adding-player", (data) => {
+        console.log(data);
+        socket.emit("NewUser-adding",data)
+      });
+
+    socket.on("disconnect",()=>{
+        console.log("Disconnected")
+        players = {}
+        socket.disconnect()
+    })
+})
+
+io.listen(5000,()=>{
+    console.log("io is listening on port 5000")
+})
